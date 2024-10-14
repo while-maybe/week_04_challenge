bab2b35367d7be34ee9473ed9693e845ad0e9dde
username <178353419+while-maybe@users.noreply.github.com>
2024-10-14 16:56:26 +0100

    feat: show specific task buttons depending on status

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const delRowEls = document.getElementsByClassName("delete-btn");
 
-  const task1 = {
-    status: "Incomplete",
-    title: "Complete user dashboard",
-    date: "2024-09-23",
-  };
-
-  const task2 = {
-    status: "Incomplete",
-    title: "A second task",
-    date: "2024-09-01"
-  };
-
   const allTasks = JSON.parse(localStorage.getItem('allTasks')) || [];
 
   addButtonEl.addEventListener("click", () => createTaskEl(newTaskEl));
@@ -70,8 +58,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function createTaskEl(newTaskEl) {
     // create a "clean title from the user input"
-    
+
     const new_title = newTaskEl.value.trim();
+
 
     if (!taskExists(newTaskEl) && new_title) {
       const row = document.createElement("tr");
@@ -103,18 +92,30 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   function loadTasks(tasks = allTasks) {
-    
+
     let t = {};
     for (t of tasks) {
+
+      // restore the type of buttons to display between page reloads depending on if the status of the task is "incomplete" or not.
+      const rowButtons = t.status.toLowerCase() === "incomplete"
+      ?
+        `<button class="edit-btn">Edit</button>
+        <button class="complete-btn" > Complete</button >
+        <button class="delete-btn">Delete</button>` 
+      :
+        `<button class="restore-btn">Restore</button>
+        <button class="delete-btn">Delete</button>`;
+
+      // creates a new row and populates the tds
       const row = document.createElement("tr");
       row.innerHTML = `<td>${t.status}</td>
                         <td>${t.title}</td>
                         <td>${t.date}</td>
-                        <td><button class="edit-btn">Edit</button>
-                        <button class="complete-btn">Complete</button>
-                        <button class="delete-btn">Delete</button></td>`;
+                        <td>${rowButtons}</td>`
       tableDataEls.appendChild(row);
     };
   };
+
+  // calls loadTasks to add them to the page table
   loadTasks();
 });
